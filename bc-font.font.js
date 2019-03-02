9a5e11896887d55749c607bf0af953cb2889c177
Bertrand Choubert <bertrand.choubert@hotmail.fr>
2019-03-02 13:23:38 +0100

    Migration to WebPack

---
+++ bc-font.font.js
@@ -0,0 +1,11 @@
+module.exports = {
+  'files': [
+    './original/processed/*.svg'
+  ],
+  'fontName': 'bc-font',
+  'classPrefix': 'icon-',
+  'baseSelector': '.icon',
+  'types': ['eot', 'woff', 'woff2', 'ttf', 'svg'],
+  'fixedWidth': true,
+  'fileName': '[fontname].[ext]'
+};
---
+++ entry.js
@@ -0,0 +1,1 @@
+require('./bc-font.font');
